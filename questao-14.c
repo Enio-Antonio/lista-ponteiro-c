b7e1890f54d3151f26399183592eb02666579af4
Ênio <146883699+Enio-Antonio@users.noreply.github.com>
2024-03-19 21:37:28 -0300

    Formatação questão 14
    Correção de erros de formatação.

--- questao-14.c
+++ questao-14.c
@@ -1,4 +1,8 @@
-// "Ponteiros para funções podem ser passados como argumentos para outras funções, e a função apontada pode ser invocada a partir de seu      // ponteiro. Um exemplo prático desta capacidade é seu uso em uma rotina de ordenação de elementos de um arranjo.[..] Ponteiros para funções // tornam-se interessantes quando o programador não pode determinar qual função deve ser executada em uma dada situação a não ser durante a // execução do programa. Em tais casos, o trecho do programa referenciando esta ``função variável'' pode ser escrito em termos de ativação // de uma função através de ponteiros para funções, os quais são corretamente inicializados em tempo de execução."
+// "Ponteiros para funções podem ser passados como argumentos para outras funções, e a função apontada pode ser invocada a partir de seu      
+// ponteiro. Um exemplo prático desta capacidade é seu uso em uma rotina de ordenação de elementos de um arranjo.[..] Ponteiros para funções 
+// tornam-se interessantes quando o programador não pode determinar qual função deve ser executada em uma dada situação a não ser durante a 
+// execução do programa. Em tais casos, o trecho do programa referenciando esta ``função variável'' pode ser escrito em termos de ativação 
+// de uma função através de ponteiros para funções, os quais são corretamente inicializados em tempo de execução."
 
 // Referência: https://www.dca.fee.unicamp.br/cursos/EA876/apostila/HTML/node144.html, Ivan L. M. Ricarte 2003-02-14
 
