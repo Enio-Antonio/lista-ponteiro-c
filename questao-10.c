731984443a334b9020651cde84837812eab6ac07
DaityaKhohe <116195128+DaityaKhohe@users.noreply.github.com>
2024-03-22 07:55:26 -0300

    Update questao-10.c

--- questao-10.c
+++ questao-10.c
@@ -4,4 +4,4 @@ void main() {
 	printf("%d", ++(*x));
 }
 
-// Não é possível compilar esse programa sem erros. Como x é uma variável do tipo int const *, não é possível inicializá-la com o valor 3, além disso, não é possível somar um inteiro a ela, já x é do tipo const. 
+// Não é possível compilar esse programa sem erros. Como x é uma variável do tipo int const *, não é possível inicializá-la com o valor 3, além disso, não é possível somar um inteiro a ela, já x que é do tipo const. 
