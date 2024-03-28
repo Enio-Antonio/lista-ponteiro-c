dff0953a15813bfcfdcb18fced5fd8070a10ed6a
DaityaKhohe <116195128+DaityaKhohe@users.noreply.github.com>
2024-03-28 18:07:57 -0300

    Questão 24

--- questao-24.c
+++ questao-24.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 int main() {
     char **m;
+    char **p;
     long estado = 0;
     int n = 8;
     m = malloc(n*sizeof(char*));
     m[0]=malloc(n*n*sizeof(char));
-    for(int i = 1; i<n;i++){
+    for(int i = 1;i<n;i++){
         m[i]=m[i-1]+n;
     }
+    p = malloc(n*sizeof(char*));
+    p[0]=malloc(n*n*sizeof(char));
+    for(int i = 1;i<n;i++){
+        p[i]=p[i-1]+n;
+    }
     for(int i = 0;i<n;i++){
-        for(int j = 0; j<n;j++){
+        for(int j = 0;j<n;j++){
             if(j%2==0){
                 m[i][j] = 1;
             }
@@ -19,9 +25,9 @@ int main() {
             }
         }
     }
-    printf("Matriz inicial:\n");
+    printf("Matriz que será atribuida ao long:\n");
     for(int i = 0;i<n;i++){
-        for(int j = 0; j<n;j++){
+        for(int j = 0;j<n;j++){
             printf("%d ",m[i][j]);
         }
         printf("\n");
@@ -33,20 +39,22 @@ int main() {
         }
     }
     //Operação Inversa
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            m[i][j] = estado & 1; // caso o valor em "estado" seja 1, o valor naquela posição será 1. Senão, será 0. Operação "AND"
+    for (int i = n-1;i>=0;i--) {
+        for (int j = n-1;j>=0;j--) {
+            p[i][j] = estado & 1; // caso o valor em "estado" seja 1, o valor naquela posição será 1. Senão, será 0. Operação "AND"
             estado = estado >> 1; //desloca os bits de estado para a direta em uma posição.
         }
     }
-    printf("Matriz atualizada:\n");
+    printf("Matriz resultante do long:\n");
     for(int i = 0;i<n;i++){
-        for(int j = 0; j<n;j++){
-            printf("%d ",m[i][j]);
+        for(int j = 0;j<n;j++){
+            printf("%d ",p[i][j]);
         }
         printf("\n");
     }
     free(m[0]);
     free(m);
+    free(p[0]);
+    free(p);
     return 0;
 }
