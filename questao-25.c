d5a2405af1639115a9df0054a46d22f31d65c049
Ênio <146883699+Enio-Antonio@users.noreply.github.com>
2024-04-02 22:53:15 -0300

    Update questao-25.c
    Adição do while no código.

--- questao-25.c
+++ questao-25.c
@@ -10,6 +10,7 @@ int main(){
   int nl, nc, np;
   int acrescimo = 1;
   int resposta;
+  int saida = 1;
 
   printf("Digite o número de linhas da matriz: ");
   scanf("%d", &nl);
@@ -47,52 +48,57 @@ int main(){
   }
 
   // Interação com o usuário
-  printf("O você deseja imprimir?\n 1 para plano ou 0 para linha.\nSe quiser mudar o estado de um elemento digite -1\n");
-  scanf("%d", &resposta);
-
+  while(saida != 0){
+    printf("O você deseja imprimir?\n 1 para plano ou 0 para linha.\nSe quiser mudar o estado de um elemento digite -1\n");
+    scanf("%d", &resposta);
   
-  if (resposta == 1){
-    printf("Qual plano você deseja imprimir (lembre-se de que as posições são reduzidas em 1)?\n");
-    int plano;
-    scanf("%d", &plano);
-    for(int i = 0; i < nl; i++){
+  
+    if (resposta == 1){
+      printf("Qual plano você deseja imprimir (lembre-se de que as posições são reduzidas em 1)?\n");
+      int plano;
+      scanf("%d", &plano);
+      for(int i = 0; i < nl; i++){
+        for(int c = 0; c < nc; c++){
+          printf("%d ", matriz[i][c][plano]);
+        } 
+        printf("\n");
+      }
+      printf("\n");
+    }
+    else if(resposta == 0){
+  
+      printf("Qual linha você deseja imprimir (lembre-se de que as posições são reduzidas em 1)?\n");
+  
+      int linha;
+      scanf("%d", &linha);
+  
       for(int c = 0; c < nc; c++){
-        printf("%d ", matriz[i][c][plano]);
-      } 
-      printf("\n");
+        for(int p = 0; p < np; p++){
+          printf("%d ", matriz[linha][c][p]);
+        } 
+        printf("\n");
+      }
+  
     }
-    printf("\n");
-  }
-  else if(resposta == 0){
-    
-    printf("Qual linha você deseja imprimir (lembre-se de que as posições são reduzidas em 1)?\n");
-    
-    int linha;
-    scanf("%d", &linha);
+    else if(resposta == -1){
+  
+      int c1, c2, c3, estado;
+  
+      printf("Digite as coordenadas no formato: linha coluna plano\n");
+      scanf("%d %d %d", &c1, &c2, &c3);
+      printf("Digite o estado:\n");
+      scanf("%d", &estado);
+  
+      if (estado != 1 && estado != 0){
+        printf("Forneça um valor válido para o estado.");
+      }
+      else{
+        matriz[c1][c2][c3] = estado;
+      }
+    }
 
-    for(int c = 0; c < nc; c++){
-      for(int p = 0; p < np; p++){
-        printf("%d ", matriz[linha][c][p]);
-      } 
-      printf("\n");
-    }
-    
-  }
-  else if(resposta == -1){
-  
-    int c1, c2, c3, estado;
-  
-    printf("Digite as coordenadas no formato: linha coluna plano\n");
-    scanf("%d %d %d", &c1, &c2, &c3);
-    printf("Digite o estado:\n");
-    scanf("%d", &estado);
-  
-    if (estado != 1 && estado != 0){
-      printf("Forneça um valor válido para o estado.");
-    }
-    else{
-      matriz[c1][c2][c3] = estado;
-    }
+    printf("Digite 0 para finalizar o programa ou 1 para continuar.\n");
+    scanf("%d", &saida);
   }
 
   // Liberação de memória
